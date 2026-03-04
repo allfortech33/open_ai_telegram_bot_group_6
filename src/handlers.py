b80809b1baf55894a227ae107099725517936923
all_for_2d_3d <forworkcad@gmail.com>
2026-03-04 22:33:37 +0200

    feat(random): add random command with text and image

--- src/handlers.py
+++ src/handlers.py
@@ -3,17 +3,6 @@ import logging
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from utils import send_image, send_text, load_message, show_main_menu
-from gpt import ChatGPTService
-from config import CHATGPT_TOKEN
-
-chatgpt_service = ChatGPTService(CHATGPT_TOKEN)
-
-import logging
-
-from telegram import Update
-from telegram.ext import ContextTypes
-
 from config import CHATGPT_TOKEN
 from gpt import ChatGPTService
 from utils import (send_image, send_text, load_message, show_main_menu, load_prompt, send_text_buttons)
@@ -74,5 +63,3 @@ async def random_button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
         await random(update, context)
     elif data == 'start':
         await start(update, context)
-        }
-    )
